c3f472ebc17bffb2985f083397e76b58a529fbe6
Lecton <simyucen12@gmail.com>
2025-07-29 00:10:21 +0800

    fixing images again

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ connection.connect((err) => {
 // Set up view engine
 app.set('view engine', 'ejs');
 //  enable static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // enable form processing
 app.use(express.urlencoded({
     extended: false
